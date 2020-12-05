b6c2b03897479eebbf5a378644c5f51073451ec1
thiagovvb <thiagovvb@gmail.com>
2020-12-05 18:35:14 -0300

    So falta CPD

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import json
+import json as js
 import math
 
 #COMPUTE P(a_i,a_j|c) from D
@@ -19,11 +19,6 @@ def calc_cond_prob(dataset,up,down = []):
 
     query_up = (" & ".join(query_up))
     query_down = (" & ".join(query_down))
-
-    #print(query_up, query_down)
-    #print(dataset.query(query_up))
-    #print(dataset.query(query_down))
-    #print("------------------")
 
     if(not query_down):
         return (len(dataset.query(query_up))/len(dataset))
@@ -56,6 +51,10 @@ def calc_info_gain(dataset, var1, var2, var3):
 
     return sumval
 
+    def calcula_cpd(dataset, weights_pair, var, classvar):
+        
+
+
 def main(filepath, classvar):
 
     #A SET D OF TRAINING EXAMPLES
@@ -69,12 +68,15 @@ def main(filepath, classvar):
         p_c[i] = value_counts[i]/len(dataset)
 
     info_gains = {}
+    weights_single = {}
+    weights_pair = {}
 
     print("Calculando ganhos de informação...")
 
     try:
-        with open(filepath.split("/")[-1] + '.json', 'r') as f:
-            info_gains = json.load(f)
+        with open("./" + filepath.split("/")[-1] + '.json', 'r') as f:
+            info_gains = js.load(f)
+        print("Arquivo já existente. Carregando valores..")
     except:
         n_inter = len(dataset.columns) * len(dataset.columns)
         n_inter_atual = 0
@@ -85,12 +87,37 @@ def main(filepath, classvar):
                 if(n_inter_atual % 10 == 0):
                     prog = (n_inter_atual/n_inter)*100
                     print("Progresso: ", n_inter_atual, " de ", n_inter, "(",  int(prog), "%)" )
-                info_gains[i,j] = calc_info_gain(dataset, i,j,classvar)
+
+                if(str(j + " " + i) not in info_gains):
+                    info_gains[i + " " + j] = calc_info_gain(dataset, i,j,classvar)
+                else:
+                    info_gains[i + " " + j] = info_gains[j + " " + i]
         
-        json = json.dumps(info_gains)
+        json = js.dumps(info_gains)
         f = open(filepath.split("/")[-1] + '.json',"w")
         f.write(json)
         f.close()
+
+    #Compute Wi
+    #Compute Wij
+    
+    print("Calculando pesos single...")
+    for i in dataset.columns:
+        sum_val = 0
+        for j in dataset.columns:
+            if(not j == i and not j == classvar and not i == classvar):
+                sum_val = sum_val + info_gains[i + " " + j]
+        weights_single[i] = sum_val
+
+    print("Calculando pesos par...")
+    for i in dataset.columns:
+        for j in dataset.columns:
+            if(not j == i and not j == classvar and not i == classvar):
+                if(weights_single[i] == 0):
+                    weights_pair[i + " " + j] = info_gains[i + " " + j]
+                else:
+                    weights_pair[i + " " + j] = info_gains[i + " " + j]/weights_single[i]
+
     
     
     
